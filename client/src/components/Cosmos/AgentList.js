42804056d9e8c55c1551495a7ba4d92ed4f3e393
Kasey Hagi <khagi@hsfl.hawaii.edu>
2018-12-21 13:53:52 -1000

    AgentList shows if agent is currently active

--- client/src/components/Cosmos/AgentList.js
+++ client/src/components/Cosmos/AgentList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from 'antd';
+import { Table, Badge } from 'antd';
 import Navbar from './../Global/Navbar';
 import io from 'socket.io-client';
 import cosmosInfo from './CosmosInfo'
@@ -31,6 +31,10 @@ const columns = [{
   key: 'agent_utc',
   width: '16%'
 }, {
+  title: 'AgentStatus',
+  dataIndex: 'status',
+  key: 'status',
+  width: '16%'
 }];
 
 class AgentList extends Component {
@@ -47,21 +51,41 @@ class AgentList extends Component {
           agents:data.result
         })
       );
+      socket.on('agent update list', (data) => { // subscribe to agent
+        if (data) {
+          console.log(data)
+          var agents = this.state.agents;
+          for(var i=0; i < agents.length; i++){
+              if(data[agents[i].agent_proc]) {
+                // console.log( agents[i].agent_proc," live")
+                agents[i].live=true;
+              } else {
+                agents[i].live=false;
+              }
+          }
+          this.setState({agents:agents})
+        }
+      });
     }
 
     render() {
       var agent_list  = this.state.agents;
+      console.log(this.state.agents)
       // var keys = Object.keys(agent_list);
       var data=[]
       for(var i =0; i < agent_list.length; i++){
+        var status=<Badge status="default" />;
+        if( agent_list[i].live===true){
+          status= <Badge status="success" />
+        }
         data[i]={
           key : String(i),
           agent_proc: String(agent_list[i].agent_proc),
           agent_node: agent_list[i].agent_node,
           agent_addr: agent_list[i].agent_addr,
           agent_port: agent_list[i].agent_port,
-          agent_utc: agent_list[i].agent_utc
-
+          agent_utc: agent_list[i].agent_utc,
+          status: status
         };
       }
       return (
