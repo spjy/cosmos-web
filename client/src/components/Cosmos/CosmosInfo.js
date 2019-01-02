60f99ff851f0ae1981ed17976dd0bdd29194cc1c
Kasey Hagi <khagi@hsfl.hawaii.edu>
2019-01-02 12:17:25 -1000

    changed ip 23-> 103

--- client/src/components/Cosmos/CosmosInfo.js
+++ client/src/components/Cosmos/CosmosInfo.js
@@ -1,5 +1,5 @@
 const cosmosInfo = {
-  hostname:'192.168.150.23',
-  socket: 'http://192.168.150.23:3001'
+  hostname:'192.168.150.103',
+  socket: 'http://192.168.150.103:3001'
 };
  export { cosmosInfo as default}
