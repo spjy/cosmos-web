9888be8629c944f9db9779a302cb333559bf76aa
Kasey Hagi <khagi@hsfl.hawaii.edu>
2019-02-05 16:20:58 -1000

    fixed widget delete

--- client/src/components/Cosmos/CosmosInfo.js
+++ client/src/components/Cosmos/CosmosInfo.js
@@ -1,7 +1,4 @@
-// const cosmosInfo = {
-//   hostname:'192.168.150.103',
-//   socket: 'http://192.168.150.103:3001'
-// };
+
 const cosmosInfo = {
   hostname:'localhost',
   socket: 'http://localhost:3001'
