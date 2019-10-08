375b6db5753da3dfcabb4f19d04cb3255abcee1a
Spencer <spjy@hawaii.edu>
2019-10-08 13:07:52 -1000

    feat: implement satellite passes table

---
+++ src/components/Global/Table.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Badge } from 'antd';
+
+function Table({
+  fullWidth,
+  columns,
+  data,
+}) {
+  return (
+    <table className="w-full">
+      <style jsx>
+        {`
+          th, td {
+            padding: 4px 15px;
+          }
+        `}
+      </style>
+      <tbody>
+        <tr className="head border-gray-300 border-b overflow-scroll-auto">
+          {
+            columns.map(({ title }) => (
+              <th key={title}>{title}</th>
+            ))
+          }
+        </tr>
+        {
+          data.map((row, i) => (
+            <tr key={i}>
+              {columns.map(({ key }) => (
+                <td>{row[key]}</td>
+              ))}
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  fullWidth: PropTypes.bool,
+  columns: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    key: PropTypes.string,
+  })),
+  data: PropTypes.arrayOf(
+    PropTypes.shape({}),
+  ),
+}
+
+Table.defaultProps = {
+  fullWidth: true,
+  columns: [],
+  data: [],
+};
+
+export default Table;
