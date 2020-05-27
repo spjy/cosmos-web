9d6a0682989b9f5e1986185f55eea211eb0ad71b
spjy <spjy@hawaii.edu>
2020-05-26 15:20:05 -1000

    style: estlint

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment-timezone';
 
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
 
@@ -12,25 +12,25 @@ export default class MyScheduler extends Component {
       {
         start: moment().toDate(),
         end: moment()
-            .add(1, "days")
-            .toDate(),
-        title: "Some title"
-      }
-    ]
+          .add(1, 'days')
+          .toDate(),
+        title: 'Some title',
+      },
+    ],
   };
 
   render() {
     return (
-        <div className="App">
-          <Calendar
-              localizer={localizer}
-              defaultDate={new Date()}
-              defaultView="month"
-              selectable
-              events={this.state.events}
-              style={{ height: "100vh" }}
-          />
-        </div>
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          selectable
+          events={this.state.events}
+          style={{ height: '100vh' }}
+        />
+      </div>
     );
   }
 }
