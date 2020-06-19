6d96056bc62b12889e9b005f694be68822f40b7f
spjy <spjy@hawaii.edu>
2020-06-19 09:54:10 -1000

    chore: update tailwindcss

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -23,6 +23,25 @@ module.exports = {
       html: {
         title: 'COSMOS Web',
       },
+      style: {
+        // Override the default file extension of `.css` if needed
+        test: /\.(css|sass|scss)$/,
+        modulesTest: /\.module\.(css|sass|scss)$/,
+        loaders: [
+          // Define loaders as objects. Note: loaders must be specified in reverse order.
+          // ie: for the loaders below the actual execution order would be:
+          // input file -> sass-loader -> postcss-loader -> css-loader -> style-loader/mini-css-extract-plugin
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        ],
+      },
     }),
     jest({
       setupFiles: [
