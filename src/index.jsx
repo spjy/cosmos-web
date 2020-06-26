6f6af87228e271934a7542ffc484cc4373427e38
spjy <spjy@hawaii.edu>
2020-06-25 15:24:02 -1000

    feat: set up redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
 import App from './App';
+
+import store from './store';
+
 import './App.css';
 
 const load = () => render(
   (
     <AppContainer>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AppContainer>
   ),
   document.getElementById('root'),
