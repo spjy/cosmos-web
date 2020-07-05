b4e1a0536342bf137aed720cbce3ed9ab214ddb5
Spencer Young <spjy@hawaii.edu>
2020-07-04 18:08:44 -1000

    fix: catch all route to allow page reload

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all route exposing compiled React app
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', '/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
+// Listen on specified port & notify
 app.listen(8080, () => console.log('Listening on http://localhost:8080'));
