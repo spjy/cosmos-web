6d1f8cf0c528277b59352fcbd85446b7e5ad3d19
spjy <spjy@hawaii.edu>
2020-07-09 10:36:18 -1000

    feat: add mjd to js date

--- src/utility/time.js
+++ src/utility/time.js
@@ -10,3 +10,9 @@ export function mjdToString(mjd) {
 export function dateToMJD(date) {
   return (date.unix() / 86400.0) + 2440587.5 - 2400000.5;
 }
+
+export function MJDtoJavaScriptDate(mjd) {
+  return dayjs
+    .unix((((mjd + 2400000.5) - 2440587.5) * 86400.0))
+    .toDate();
+}
